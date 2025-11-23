7994385648a65d8d92ef595deb745601529169d1
XxAlvarozZ <167907888+XxAlvarozZ@users.noreply.github.com>
2025-11-23 00:49:00 -0500

    La estructura del Frontend - Sistemas  Perros

---
+++ albergue-frontend/src/app/components/donacion/donacion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donacion',
+  imports: [],
+  templateUrl: './donacion.html',
+  styleUrl: './donacion.css',
+})
+export class Donacion {
+
+}
---
+++ albergue-frontend/src/app/components/registrar-perro/registrar-perro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-perro',
+  imports: [],
+  templateUrl: './registrar-perro.html',
+  styleUrl: './registrar-perro.css',
+})
+export class RegistrarPerro {
+
+}
