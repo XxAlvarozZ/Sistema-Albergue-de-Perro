cdb04021e160b6ba0f60cce1464b70f8ddcfc021
Diego60830116 <167909288+Diego60830116@users.noreply.github.com>
2025-11-23 22:35:56 -0500

    ...

--- backend/app/services/donaciones_service.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Session
-from .. import repository, schemas
-
-def crear_donacion(db: Session, donacion_in: schemas.DonacionCreate):
-    return repository.donaciones_repo.crear_donacion(db, donacion_in)
-
-def listar_donaciones(db: Session, skip:int=0, limit:int=100):
-    return repository.donaciones_repo.listar_donaciones(db, skip, limit)
